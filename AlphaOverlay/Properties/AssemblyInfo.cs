eb5460faec3083f548e1534adebd5744162ca50e
Paralleltree <paralleltree@outlook.com>
2017-09-30 20:09:24 +0900

    Add a new project

---
+++ AlphaOverlay/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlphaOverlay")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("paltee.net")]
+[assembly: AssemblyProduct("AlphaOverlay")]
+[assembly: AssemblyCopyright("Copyright Paralleltree 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4d7c51de-78f9-416f-90c1-fd54e33adc68")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
